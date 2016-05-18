51d96d129729e08d51297ae97bfbd44e5ea581a6
Wei Chen <wei.chen@harcourts.net>
2016-05-18 16:51:22 +1200

    Add ProjectOxford
    Add ProjectOxford

---
+++ Recognition.ProjectOxford/FaceService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Face;
+
+namespace Harcourts.Face.Recognition.ProjectOxford
+{
+    public class FaceService
+    {
+        private static readonly string SubscriptionKey;
+
+        static FaceService()
+        {
+            SubscriptionKey = ConfigurationManager.AppSettings["ProjectOxford.Face.SubscriptionKey"];
+        }
+    }
+}
--- Recognition.ProjectOxford/Properties/AssemblyInfo.cs
+++ Recognition.ProjectOxford/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Recognition")]
+[assembly: AssemblyTitle("Recognition.ProjectOxford")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Recognition")]
+[assembly: AssemblyProduct("Recognition.ProjectOxford")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4ffca8b2-674b-4d2e-911c-ccce7bdb5784")]
+[assembly: Guid("7e691e90-73f5-43db-aaad-1b4cff8ae7c1")]
 
 // Version information for an assembly consists of the following four values:
 //
